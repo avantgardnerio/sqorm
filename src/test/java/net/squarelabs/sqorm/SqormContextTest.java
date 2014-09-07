b2a6cd5c7e84c8fa66d73b7d047c2683c648dd97
Brent Gardner <bgardner@squarelabs.net>
2014-09-07 13:01:10 -0600

    Added junit

---
+++ src/test/java/net/squarelabs/sqorm/SqormContextTest.java
@@ -0,0 +1,18 @@
+package net.squarelabs.sqorm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/*
+ * sqorm
+ * https://github.com/bgard6977/sqorm
+ *
+ * Copyright (c) 2014 Brent Gardner
+ * Licensed under the MIT license.
+ */
+public class SqormContextTest {
+    @Test
+    public void canInstantiate() {
+        Assert.assertTrue(false);
+    }
+}
