4a149ce1f7e2ba1e2d392eb6e7c8fc14dbb4013a
Brent Gardner <bgardner@squarelabs.net>
2014-09-07 17:57:13 -0600

    Checking in MySQL schema 1.0

---
+++ src/test/resources/ddl/mysql.sql
@@ -0,0 +1,18 @@
+USE `sqorm`;
+
+-- customer
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- orders
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `OrderId` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  PRIMARY KEY (`OrderId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
